fd68237e8df856e2da866b0f301b0e94f3796999
Guilherme Baufaker <baufaker@gmail.com>
2011-11-01 18:55:21 -0200

    Primeiro commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+gem 'sqlite3'
+gem 'slim'
+gem 'compass',             '~> 0.12.alpha.0'
+gem 'sass-rails',          '~> 3.1.4'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'cucumber-rails'
+  gem 'capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IMC::Application.config.secret_token = '25efead1c0b9050d2211a40f5c448eae82d2963ecd78b4e0f2d0e573caefc3de2fda4d309e97893eeac8069730dbd52ea33e87f5011b8faad3912c46c81e8d59'
